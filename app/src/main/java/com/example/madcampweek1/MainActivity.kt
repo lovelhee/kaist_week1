e6858448d5fe2a8333e5f8519ba8c5eeb410640b
김정희 <jenny8530@naver.com>
2024-12-27 11:59:36 +0900

    feat: establish initial app structure

--- app/src/main/java/com/example/madcampweek1/MainActivity.kt
+++ app/src/main/java/com/example/madcampweek1/MainActivity.kt
@@ -5,16 +5,50 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.madcampweek1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // View Binding 초기화
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 초기 화면 설정
+        replaceFragment(ContactsFragment())
+
+        // 바텀 네비게이션 아이템 클릭 리스너 설정
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_contacts -> {
+                    replaceFragment(ContactsFragment())
+                    true
+                }
+                R.id.navigation_gallery -> {
+                    replaceFragment(GalleryFragment())
+                    true
+                }
+                R.id.navigation_help -> {
+                    replaceFragment(HelpFragment())
+                    true
+                }
+                else -> false
+            }
         }
+
+        // 툴바 설정
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
     }
 }
