53e40820bd14f31c269ed79ed0f06e6eac863a5e
김정희 <jenny8530@naver.com>
2024-12-28 14:26:23 +0900

    feat: add grid layout dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
 
     implementation("com.google.code.gson:gson:2.8.9")
     implementation("com.github.bumptech.glide:glide:4.15.1")
+    implementation(libs.androidx.gridlayout)
     kapt("com.github.bumptech.glide:compiler:4.15.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
