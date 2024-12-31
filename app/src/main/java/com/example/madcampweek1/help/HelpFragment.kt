61abe29bf3787024eb009773e5c312d5c658e16b
김정희 <jenny8530@naver.com>
2024-12-31 12:10:14 +0900

    feat: complete basic structure and design for HelpFragment

--- app/src/main/java/com/example/madcampweek1/help/HelpFragment.kt
+++ app/src/main/java/com/example/madcampweek1/help/HelpFragment.kt
@@ -1,10 +1,11 @@
-package com.example.madcampweek1
+package com.example.madcampweek1.help
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.madcampweek1.R
 
 class HelpFragment : Fragment() {
     override fun onCreateView(
